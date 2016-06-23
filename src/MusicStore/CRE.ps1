42189ab798fde62aa6f075719a2aeeafc6b47766
Red Hat Developers <Red Hat Developers>
2016-06-23 12:37:22 -0400

    Made the powershell script a bit more power/automated

--- src/MusicStore/CRE.ps1
+++ src/MusicStore/CRE.ps1
@@ -27,4 +27,15 @@ $CREBinFolder = Join-Path $ProjectFolder (Join-Path "bin" "CRE")
 $buildContext = Join-Path $CREBinFolder $Configuration
 $pubPath = Join-Path $buildContext "app"
 dotnet publish -f $Framework -r "rhel.7.2-x64" -c $Configuration -o $pubPath $ProjectFolder
-C:\cdk\docker-1.9.1.exe run -d -p 5000:80 -v "/c/Users/RedHatDevelopers/Documents/Repos/MusicStore/src/MusicStore/bin/CRE/Debug/app:/app" qodfathr/rhel-dotnet:1.0-debug
+$existingDotnetContainers = $(C:\cdk\docker-1.9.1.exe ps -a | Select-String -pattern ":5000->80" | foreach {Write-Output $_.Line.split()[0]})
+if ($existingDotnetContainers)
+{
+	$ids = $existingDotnetContainers -join ' '
+	Invoke-Expression "cmd /c c:\cdk\docker-1.9.1.exe stop $ids `"2>&1`""
+}
+$debugContainerId = $(C:\cdk\docker-1.9.1.exe run -d -p 5000:80 -v "/c/Users/RedHatDevelopers/Documents/Repos/MusicStore/src/MusicStore/bin/CRE/Debug/app:/app" qodfathr/rhel-dotnet:1.0-debug)
+$dockerIp = ($env:DOCKER_HOST -replace "tcp://", "" -split(":"))[0]
+$launchOpts = Get-Content "C:\cdk\launchOptions.template"
+$launchOpts = $launchOpts -replace "<containerid>", $debugContainerId
+Set-Content "C:\cdk\launchOptions.xml" $launchOpts
+Start-Process "http://${dockerIp}:5000"
